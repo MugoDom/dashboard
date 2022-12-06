9df004c28fb16f4310d93fc250ea08fbfa01556b
denis <denismunene999@gmail.com>
2022-12-06 09:44:57 +0300

    style map component

---
+++ src/components/map/RenderMap.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import { useRef } from 'react';
+import { MapContainer, TileLayer } from 'react-leaflet'
+import osm from './Osm-providers';
+import "leaflet/dist/leaflet.css";
+import './Map.css'
+
+function RenderMap() {
+  const [center, setCenter] = useState({lat: 0.0236, lng: 37.9062});
+  const ZOOM_LEVEL = 6;
+  const mapRef = useRef();
+
+
+  return (
+    <div className='leaftlet-container' >
+      <p>Loading map using basic layer from map tiler</p>
+      <MapContainer
+        center={center}
+        zoom={ZOOM_LEVEL}
+        ref={mapRef}
+      >
+        <TileLayer url={osm.maptiler.url} attribution={osm.maptiler.attribution} />
+      </MapContainer>
+    </div>
+  )
+}
+
+export default RenderMap
--- src/pages/countries/kenya/Kenya.jsx
+++ src/pages/countries/kenya/Kenya.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from 'react';
+import './kenya.scss';
+import Navbar from '../../../components/navbar/Navbar';
+import Sidebar from '../../../components/sidebar/Sidebar';
+import RenderMap from '../../../components/map/RenderMap';
 
 const Kenya = () => {
   return (
-    <div className='widget'>
-      <div className='left'>Left</div><div/>
-      <div className='left'>Right</div><div/>
+    <div className='kenya'>
+      <Sidebar />
+      <div className='kenya-container'>
+        <Navbar />
+        <p>Hello from kenya</p>
+        <RenderMap />
+      </div>
     </div>
   )
 }
